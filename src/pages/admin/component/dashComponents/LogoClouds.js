b2ec18cbc8f151b4aa09c765c4c1c40b7b7cbe19
Abid <meghpiash@gmail.com>
2023-03-07 14:33:07 +0600

    projects json added

--- src/pages/admin/component/dashComponents/LogoClouds.js
+++ src/pages/admin/component/dashComponents/LogoClouds.js
@@ -1,66 +1,57 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 export default function LogoClouds() {
-    const [isOpen, setIsOpen] = useState(false);
     const [items, setitems] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const [selectedItems, setSelectedItems] = useState([]);
-    // fetch blogs data from json
-    useEffect(() => {
+    
+    useEffect(()=>{
         setIsLoading(true);
         fetch(`data/blogs.json`)
-            .then(response => response.json())
-            .then(data => {
+            .then(res=>res.json())
+            .then(data=>{
                 setitems(data);
                 setIsLoading(false);
             })
-            .catch(error => console.log('Error fetching data:', error));
-    }, []);
-    if (isLoading) {return <div>Loading...</div>;}
-    if (!items) {return <div>No data found</div>; }
-
-    function handleCheckboxChange(event) {
+            .then(error=>console.log('error:', error));
+    },[]);
+    function handleCheckboxChange (event) {
         const value = event.target.value;
         const isChecked = event.target.checked;
-    
-        if (isChecked) {
-          setSelectedItems([...selectedItems, value]);
-        } else {
-          setSelectedItems(selectedItems.filter(item => item !== value));
-        }
-      }
+        if (isChecked){setSelectedItems([...selectedItems, value])}
+        else {setSelectedItems(selectedItems.filter(item => item !== value))}
+    }
+
   return (
-    <div className='bg-gray-600 p-2'>
-        <div className='flex items-center justify-between'>
-            <h1>Logo Clouds</h1>
-            <div className="relative inline-block">
-                <button className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-                    onClick={()=>setIsOpen(!isOpen)}
-                    aria-expanded={isOpen}
-                    aria-haspopup="true">
-                    Select options
-                </button>
-                {(isOpen && items.length > 0) && (
-                    <div className="absolute z-10 top-10 right-0 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                        {
-                            items.map(item => (
-                            <label className="block py-2 px-4 text-sm text-gray-700">
+    <div className="bg-gray-600">
+        {isLoading && <div>Loading...</div> }
+        <div>
+            <div className="flex justify-between">
+                <h1>Logo clouds</h1>
+                <div className="relative inline-block">
+                    <button onClick={()=>setIsOpen(!isOpen)} className="py-2 px-4 bg-blue-500">
+                        Select items
+                    </button>
+                    {isOpen && items && <div className="absolute z-10 top-10 right-0 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                        {items.map(item=>(
+                            <label key={item.id} className="block py-2 px-4 text-sm text-gray-700">
                                 <input type="checkbox" name="options" value={item?.value} checked={selectedItems.includes(item?.value)} onChange={handleCheckboxChange} />
-                                {item?.title}
-                            </label>))
-                        }
+                                {item.title}
+                            </label>
+                        ))}
+                    </div>}
+                </div>
+            </div>
+            <div>
+                {items.filter(item => selectedItems.includes(item.value)).map(item=>(
+                    <div key={item.id}>
+                        <h1>{item.title}</h1>
                     </div>
-                )}
+                ))}
             </div>
         </div>
-        <div className='p-2'>
-            {items.filter(data => selectedItems.includes(data.value)).map(data=>(
-                <div key={data.id} className='bg-gray-500 mt-3 p-2 rounded-lg' >
-                    <h1>{data.title}</h1>
-                    <p>{data.author}</p>
-                </div>
-            ))}
-        </div>
+        
     </div>
   )
 }
