efff0d07674ed611ae27c5acb497c0c2ca61c38d
Abid <meghpiash@gmail.com>
2023-03-08 14:05:08 +0600

    project setting is done

--- src/pages/admin/pages/ProjectSetting.js
+++ src/pages/admin/pages/ProjectSetting.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react'
+import useFetch from '../../../hooks/useFetch';
 
 export default function ProjectSetting() {
+    const {isLoading, data, error } = useFetch(`data/projects.json`);
     const [expandedRow, setExpandedRow] = useState(null);
+
+    isLoading && <div>Loading...</div>;
+  
+    error && <div>Error: {error.message}</div>;
+  
+    (!data || !data?.length) && <div>No data found</div>;
 
     const handleMoreClick = (id) => {
         if (expandedRow === id) {
@@ -10,62 +18,54 @@ export default function ProjectSetting() {
         setExpandedRow(id);
         }
     };
-    const tableData = [
-        { name: 'John Doe', age: 25, gender: 'Male', id: 1, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Jane Doe', age: 22, gender: 'Female', id: 2, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Bob Smith', age: 30, gender: 'Male', id: 3, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Bob Smith', age: 30, gender: 'Male', id: 4, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Bob Smith', age: 30, gender: 'Male', id: 5, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Bob Smith', age: 30, gender: 'Male', id: 6, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Bob Smith', age: 30, gender: 'Male', id: 7, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Bob Smith', age: 30, gender: 'Male', id: 8, moreInfo: 'Lorem ipsum dolor sit amet' },
-        { name: 'Bob Smith', age: 30, gender: 'Male', id: 9, moreInfo: 'Lorem ipsum dolor sit amet' },
-      ];
   return (
     <div>
         <div>
-        <table className="table-auto w-full">
+        <table className="table-col w-full">
             <thead>
                 <tr>
-                <th className="px-4 py-2 text-left">SN</th>
-                <th className="px-4 py-2 text-left">Name</th>
-                <th className="px-4 py-2 text-left">Age</th>
-                <th className="px-4 py-2 text-left">Gender</th>
-                <th className="px-4 py-2 text-left">Actions</th>
+                <th className="px-4 py-2 text-left w-10">SN</th>
+                <th className="px-4 py-2 text-left w-20">Name</th>
+                <th className="px-4 py-2 text-left w-72">Title</th>
+                <th className="px-4 py-2 text-left w-20">Platform</th>
+                <th className="px-4 py-2 text-left w-20">Category</th>
+                <th className="px-4 py-2 text-left w-20">Actions</th>
                 </tr>
             </thead>
             <tbody>
-                {tableData.map((row, index) => (
+                {data && data.map((row, index) => (
                 <React.Fragment key={row.id}>
-                    <tr className={`${index % 2 === 0 ? 'bg-gray-800' : 'bg-gray-600'} transform duration-200 hover:scale-[0.98] hover:bg-blue-900 hover:bg-opacity-10`}>
-                    <td className="px-4 py-2">{index+1}</td>
-                    <td className="px-4 py-2">{row.name}</td>
-                    <td className="px-4 py-2">{row.age}</td>
-                    <td className="px-4 py-2">{row.gender}</td>
-                    <td className="px-4 py-2">
-                        <div className="flex space-x-1">
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            View
-                        </button>
-                        <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-                            Edit
-                        </button>
-                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                            Delete
-                        </button>
-                        <button
-                            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                            onClick={() => handleMoreClick(row.id)}
-                        >
-                            More
-                        </button>
-                        </div>
-                    </td>
+                    <tr className={`${row.id % 2 === 0 ? 'bg-gray-800' : 'bg-gray-600'} transform duration-200 hover:scale-[0.99] hover:bg-blue-900 hover:bg-opacity-10`}>
+                        <td className="px-4 py-2">{index+1}</td>
+                        <td className="px-4 py-2">{row.name}</td>
+                        <td className="px-4 py-2">{row.title}</td>
+                        <td className="px-4 py-2">{row.platform}</td>
+                        <td className="px-4 py-2">{row.category}</td>
+                        <td className="px-4 py-2">
+                            <div className="flex space-x-1">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                View
+                            </button>
+                            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                Edit
+                            </button>
+                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                Delete
+                            </button>
+                            <button
+                                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                                onClick={() => handleMoreClick(row.id)}
+                            >
+                                More
+                            </button>
+                            </div>
+                        </td>
                     </tr>
                     {expandedRow === row.id && (
-                    <tr className="bg-gray-500 mx-4 transform duration-200">
+                    <tr className="w-full bg-gray-500">
                         <td colSpan="4" className="px-4 py-2">
-                        {row.moreInfo}
+                            <h1>Discription : {row.description}</h1>
+                            <h1>Launch Date : {row.date}</h1>
                         </td>
                     </tr>
                     )}
