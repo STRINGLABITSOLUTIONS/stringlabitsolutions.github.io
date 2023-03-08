c66acf3cc1d38830b140aa41f4a92ba31b528c87
Abid <meghpiash@gmail.com>
2023-03-08 17:41:07 +0600

    Blogsetting is now connected with json file

--- src/pages/admin/pages/BlogSetting.js
+++ src/pages/admin/pages/BlogSetting.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import useFetch from '../../../hooks/useFetch';
 
 export default function BlogSetting() {
+    const {isLoading, data, error } = useFetch(`data/blogs.json`);
+
+    isLoading && <div>Loading...</div>;
+  
+    error && <div>Error: {error.message}</div>;
+  
+    (!data || !data?.length) && <div>No data found</div>;
   return (
-    <div>BlogSetting</div>
+    <div>
+        <div className='flex items-center justify-between'>
+            <h1 className='text-3xl font-bold p-3'>Total Blogs : {data.length}</h1>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create New</button>
+        </div>
+        <div>
+            <table className="table-col w-full">
+                <thead>
+                    <tr>
+                    <th className="px-4 py-2 text-left w-10">SN</th>
+                    <th className="px-4 py-2 text-left w-72">Title</th>
+                    <th className="px-4 py-2 text-left w-20">Author</th>
+                    <th className="px-4 py-2 text-left w-20">Date</th>
+                    <th className="px-4 py-2 text-left w-20">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data && data.map((row, index) => (
+                        <tr key={row.id} className={`${row.id % 2 === 0 ? 'bg-gray-800' : 'bg-gray-600'} transform duration-200 hover:scale-[0.99] hover:bg-blue-900 hover:bg-opacity-10`}>
+                            <td className="px-4 py-2">{index+1}</td>
+                            <td className="px-4 py-2">{row.title}</td>
+                            <td className="px-4 py-2">{row.author}</td>
+                            <td className="px-4 py-2">{row.date}</td>
+                            <td className="px-4 py-2">
+                                <div className="flex space-x-1">
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    View
+                                </button>
+                                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                    Edit
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                    Delete
+                                </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </div>
   )
 }
--- src/pages/admin/pages/ProjectSetting.js
+++ src/pages/admin/pages/ProjectSetting.js
@@ -21,59 +21,58 @@ export default function ProjectSetting() {
   return (
     <div>
         <div>
-        <table className="table-col w-full">
-            <thead>
-                <tr>
-                <th className="px-4 py-2 text-left w-10">SN</th>
-                <th className="px-4 py-2 text-left w-20">Name</th>
-                <th className="px-4 py-2 text-left w-72">Title</th>
-                <th className="px-4 py-2 text-left w-20">Platform</th>
-                <th className="px-4 py-2 text-left w-20">Category</th>
-                <th className="px-4 py-2 text-left w-20">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {data && data.map((row, index) => (
-                <React.Fragment key={row.id}>
-                    <tr className={`${row.id % 2 === 0 ? 'bg-gray-800' : 'bg-gray-600'} transform duration-200 hover:scale-[0.99] hover:bg-blue-900 hover:bg-opacity-10`}>
-                        <td className="px-4 py-2">{index+1}</td>
-                        <td className="px-4 py-2">{row.name}</td>
-                        <td className="px-4 py-2">{row.title}</td>
-                        <td className="px-4 py-2">{row.platform}</td>
-                        <td className="px-4 py-2">{row.category}</td>
-                        <td className="px-4 py-2">
-                            <div className="flex space-x-1">
-                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                View
-                            </button>
-                            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
-                                Edit
-                            </button>
-                            <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                                Delete
-                            </button>
-                            <button
-                                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                                onClick={() => handleMoreClick(row.id)}
-                            >
-                                More
-                            </button>
-                            </div>
-                        </td>
+            <table className="table-col w-full">
+                <thead>
+                    <tr>
+                    <th className="px-4 py-2 text-left w-10">SN</th>
+                    <th className="px-4 py-2 text-left w-20">Name</th>
+                    <th className="px-4 py-2 text-left w-72">Title</th>
+                    <th className="px-4 py-2 text-left w-20">Platform</th>
+                    <th className="px-4 py-2 text-left w-20">Category</th>
+                    <th className="px-4 py-2 text-left w-20">Actions</th>
                     </tr>
-                    {expandedRow === row.id && (
-                    <tr className="w-full bg-gray-500">
-                        <td colSpan="4" className="px-4 py-2">
-                            <h1>Discription : {row.description}</h1>
-                            <h1>Launch Date : {row.date}</h1>
-                        </td>
-                    </tr>
-                    )}
-                </React.Fragment>
-                ))}
-            </tbody>
-        </table>
-
+                </thead>
+                <tbody>
+                    {data && data.map((row, index) => (
+                    <React.Fragment key={row.id}>
+                        <tr className={`${row.id % 2 === 0 ? 'bg-gray-800' : 'bg-gray-600'} transform duration-200 hover:scale-[0.99] hover:bg-blue-900 hover:bg-opacity-10`}>
+                            <td className="px-4 py-2">{index+1}</td>
+                            <td className="px-4 py-2">{row.name}</td>
+                            <td className="px-4 py-2">{row.title}</td>
+                            <td className="px-4 py-2">{row.platform}</td>
+                            <td className="px-4 py-2">{row.category}</td>
+                            <td className="px-4 py-2">
+                                <div className="flex space-x-1">
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    View
+                                </button>
+                                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                    Edit
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                    Delete
+                                </button>
+                                <button
+                                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                                    onClick={() => handleMoreClick(row.id)}
+                                >
+                                    More
+                                </button>
+                                </div>
+                            </td>
+                        </tr>
+                        {expandedRow === row.id && (
+                        <tr className="w-full bg-gray-500">
+                            <td colSpan="4" className="px-4 py-2">
+                                <h1>Discription : {row.description}</h1>
+                                <h1>Launch Date : {row.date}</h1>
+                            </td>
+                        </tr>
+                        )}
+                    </React.Fragment>
+                    ))}
+                </tbody>
+            </table>
         </div>
     </div>
   )
