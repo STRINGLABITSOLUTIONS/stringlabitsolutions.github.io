baed4641c0bbd3cebac1c7488b5b4fa0420e7f14
Abid <meghpiash@gmail.com>
2023-03-09 13:54:37 +0600

    complete logo setting with pagiation

--- src/pages/admin/pages/LogoSetting.js
+++ src/pages/admin/pages/LogoSetting.js
@@ -1,74 +1,59 @@
-import React, { useState } from "react";
+import useFetch from '../../../hooks/useFetch';
+import ReactPaginate from 'react-paginate';
+import { useState } from 'react';
 
 export default function LogoSetting() {
+    const [currentPage, setCurrentPage] = useState(0);
+    const {isLoading, data, error } = useFetch(`data/logos.json`);
+    const PER_PAGE = 5;
+    const offset = currentPage * PER_PAGE;
+    const currentPageData = data.slice(offset, offset + PER_PAGE);
+    const pageCount = Math.ceil(data.length / PER_PAGE);
 
-    const itemsPerPage = 5;
-
-    const [currentPage, setCurrentPage] = useState(1);
-
-    const data = {
-    item1: "Lorem ipsum dolor sit amet",
-    item2: "Consectetur adipiscing elit",
-    item3: "Sed do eiusmod tempor",
-    item4: "Incididunt ut labore et dolore magna aliqua",
-    item5: "Ut enim ad minim veniam",
-    item6: "Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
-    item7: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-    item8: "Excepteur sint occaecat cupidatat non proident",
-    item9: "Sunt in culpa qui officia deserunt mollit anim id est laborum",
-    item10: "Lorem ipsum dolor sit amet",
-    item11: "Consectetur adipiscing elit",
-    item12: "Sed do eiusmod tempor",
-    item13: "Sed do eiusmod tempor",
-    item14: "Incididunt ut labore et dolore magna aliqua",
-    item15: "Ut enim ad minim veniam",
-    };
-
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(Object.keys(data).length / itemsPerPage); i++) {
-        pageNumbers.push(i);
-    }
-
-    const handleClick = e => {
-        setCurrentPage(Number(e.target.id));
-    };
-
-    const renderPageNumbers = pageNumbers.map(number => {
-        return (
-        <li key={number} id={number} onClick={handleClick} className="border border-purple-600 w-8 text-center rounded-md cursor-pointer">
-            {number}
-        </li>
-        );
-    });
-
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = Object.entries(data).slice(indexOfFirstItem, indexOfLastItem);
-
-    const renderTableData = currentItems.map(([key, value]) => {
-        return (
-        <tr key={key}>
-            <td>{key}</td>
-            <td>{value}</td>
-        </tr>
-        );
-    });
+    isLoading && <div>Loading...</div>;
+  
+    error && <div>Error from ferthing: {error.message}</div>;
+  
+    (!data || !data?.length) && <div>No data found</div>;
+    
+    function handlePageClick({ selected: selectedPage }) {
+        setCurrentPage(selectedPage);
+      }
+    
   return (
     <div>
-         <h1 className="text-3xl p-8">Table with Pagination</h1>
-      <table className="table-fixed w-full text-left m-3">
+        <h1 className="text-3xl p-8">Table with Pagination</h1>
+        <table className="table-fixed w-full text-left m-3">
         <thead className="bg-amber-400">
           <tr>
             <th>Key</th>
             <th>Value</th>
           </tr>
         </thead>
-        <tbody>{renderTableData}</tbody>
-      </table>
-        <div class="flex items-center justify-center mx-auto">
-            <ul id="page-numbers" className="flex items-center justify-center space-x-5">{renderPageNumbers}</ul>
+        <tbody>
+            {/* {renderTableData} */}
+            {currentPageData && currentPageData.map(({id, name, link})=>(
+                <tr key={id}>
+                    <td>
+                        <h1>{name}</h1>
+                    </td>
+                    <td>
+                        <img className='h-10' src={link} alt="" loading="lazy"/>
+                    </td>
+                </tr>
+            ))
+
+            }
+        </tbody>
+        </table>
+        <div>
+        <ReactPaginate
+            pageCount={pageCount}
+            onPageChange={handlePageClick}
+            containerClassName={'flex space-x-4 mx-auto'}
+            activeClassName={'border w-6 h-6 text-center rounded-full'}
+        />
         </div>
-      
     </div>
   )
 }
