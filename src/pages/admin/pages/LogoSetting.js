a3f9a64ca84208159222caafdd325109db1e2af8
Abid <meghpiash@gmail.com>
2023-03-09 14:10:51 +0600

    update logosetting

--- src/pages/admin/pages/LogoSetting.js
+++ src/pages/admin/pages/LogoSetting.js
@@ -3,9 +3,9 @@ import ReactPaginate from 'react-paginate';
 import { useState } from 'react';
 
 export default function LogoSetting() {
-    const [currentPage, setCurrentPage] = useState(0);
     const {isLoading, data, error } = useFetch(`data/logos.json`);
     const PER_PAGE = 5;
+    const [currentPage, setCurrentPage] = useState(0);
     const offset = currentPage * PER_PAGE;
     const currentPageData = data.slice(offset, offset + PER_PAGE);
     const pageCount = Math.ceil(data.length / PER_PAGE);
@@ -22,37 +22,46 @@ export default function LogoSetting() {
     
   return (
     <div>
-        <h1 className="text-3xl p-8">Table with Pagination</h1>
+        <div className='flex items-center justify-between'>
+            <h1 className='text-3xl font-bold p-3'>Total Companys : {data.length}</h1>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Create New</button>
+        </div>
         <table className="table-fixed w-full text-left m-3">
-        <thead className="bg-amber-400">
-          <tr>
-            <th>Key</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-            {/* {renderTableData} */}
-            {currentPageData && currentPageData.map(({id, name, link})=>(
-                <tr key={id}>
-                    <td>
-                        <h1>{name}</h1>
-                    </td>
-                    <td>
-                        <img className='h-10' src={link} alt="" loading="lazy"/>
-                    </td>
-                </tr>
-            ))
+            <thead className="bg-amber-400">
+            <tr>
+                <th>Company Name</th>
+                <th>Logo</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                {currentPageData && currentPageData.map(({id, name, link})=>(
+                    <tr key={id} className={`${id % 2 === 0 ? 'bg-gray-800' : 'bg-gray-600'} transform duration-200 hover:scale-[0.99] hover:bg-blue-900 hover:bg-opacity-10`}>
+                        <td>{name}</td>
+                        <td><img className='h-10' src={link} alt="" loading="lazy"/></td>
+                        <td className="px-4 py-2">
+                                <div className="flex space-x-1">
+                                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
+                                    Edit
+                                </button>
+                                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                    Delete
+                                </button>
+                                </div>
+                            </td>
+                    </tr>
+                ))
 
-            }
-        </tbody>
+                }
+            </tbody>
         </table>
         <div>
-        <ReactPaginate
-            pageCount={pageCount}
-            onPageChange={handlePageClick}
-            containerClassName={'flex space-x-4 mx-auto'}
-            activeClassName={'border w-6 h-6 text-center rounded-full'}
-        />
+            <ReactPaginate
+                pageCount={pageCount}
+                onPageChange={handlePageClick}
+                containerClassName={'flex items-center justify-center space-x-4 mx-auto'}
+                activeClassName={'border w-6 h-6 text-center rounded-full'}
+            />
         </div>
     </div>
   )
